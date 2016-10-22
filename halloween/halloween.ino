157f8996ded3f6c4180be6e55d175220aafa355f
Jonathan W. Taylor <elderling@gmail.com>
2016-10-22 15:54:10 -0500

    Added animation code.

--- halloween/halloween.ino
+++ halloween/halloween.ino
@@ -14,6 +14,15 @@ typedef struct
   animation anim;
 } dyn_pixl;
 
+void do_wait(dyn_pixl * pixl);
+void do_on(dyn_pixl * pixl);
+void do_off(dyn_pixl * pixl);
+void render_pixl(dyn_pixl * pixl);
+void update_pixls( dyn_pixl pixls[] );
+void render_pixls( dyn_pixl pixls[] );
+void random_anim(dyn_pixl * pixl);
+long random_at_most(long max) ;
+
 #define NUMBER_OF_PIXLS 3
 #define TOTAL_FRAMES 50
 #define PIN 6
@@ -46,8 +55,14 @@ dyn_pixl dyn_pixls[NUMBER_OF_PIXLS] = {
    }
 };
 
-void
-render_pixls (dyn_pixl pixls[]);
+void do_wait(dyn_pixl * pixl);
+void do_on(dyn_pixl * pixl);
+void do_off(dyn_pixl * pixl);
+void render_pixl(dyn_pixl * pixl);
+void update_pixls( dyn_pixl pixls[] );
+void render_pixls( dyn_pixl pixls[] );
+void random_anim(dyn_pixl * pixl);
+long random_at_most(long max) ;
 
 Adafruit_NeoPixel strip =
 Adafruit_NeoPixel (NUMBER_OF_PIXLS, PIN, NEO_GRB + NEO_KHZ800);
@@ -64,7 +79,8 @@ void
 loop ()
 {
   // put your main code here, to run repeatedly:
-
+      update_pixls (dyn_pixls);
+      render_pixls (dyn_pixls);
 }
 
 void
@@ -84,4 +100,104 @@ render_pixls (dyn_pixl pixls[])
 
 strip.show();
   return;
+}
+
+void
+update_pixls (dyn_pixl pixls[])
+{
+  unsigned char n;
+  for (n = 0; n < NUMBER_OF_PIXLS; n++)
+    {
+// FIXME switch?
+      if ((&dyn_pixls[n])->anim == wait)
+        {
+          do_wait (&dyn_pixls[n]);
+        }
+
+      if ((&dyn_pixls[n])->anim == on)
+        {
+          do_on (&dyn_pixls[n]);
+        }
+
+      if ((&dyn_pixls[n])->anim == off)
+        {
+          do_off (&dyn_pixls[n]);
+        }
+
+      if ((&dyn_pixls[n])->frame == (&dyn_pixls[n])->end_frame)
+        {
+          random_anim (&dyn_pixls[n]);
+        }
+    }
+
+  return;
+}
+
+void
+random_anim (dyn_pixl * pixl)
+{
+  pixl->frame = 0;
+  pixl->anim = (animation) random_at_most (2);
+  pixl->end_frame = (int) random_at_most (6);
+  return;
+}
+
+void
+do_wait (dyn_pixl * pixl)
+{
+  if (pixl->frame < pixl->end_frame)
+    {
+      pixl->frame++;
+    }
+
+  return;
+}
+
+void
+do_on (dyn_pixl * pixl)
+{
+  pixl->brightness = 100;
+  if (pixl->frame < pixl->end_frame)
+    {
+      pixl->frame++;
+    }
+
+  return;
+}
+
+void
+do_off (dyn_pixl * pixl)
+{
+  pixl->brightness = 0;
+  if (pixl->frame < pixl->end_frame)
+    {
+      pixl->frame++;
+    }
+
+  return;
+}
+
+// From http://stackoverflow.com/questions/2509679/how-to-generate-a-random-number-from-within-a-range#6852396
+// Assumes 0 <= max <= RAND_MAX
+// Returns in the closed interval [0, max]
+long
+random_at_most (long max)
+{
+  unsigned long
+    // max <= RAND_MAX < ULONG_MAX, so this is okay.
+   
+    num_bins = (unsigned long) max + 1,
+    num_rand = (unsigned long) RAND_MAX + 1,
+    bin_size = num_rand / num_bins, defect = num_rand % num_bins;
+
+  long x;
+  do
+    {
+      x = random ();
+    }
+  // This is carefully written not to overflow
+  while (num_rand - defect <= (unsigned long) x);
+
+  // Truncated division is intentional
+  return x / bin_size;
 }
