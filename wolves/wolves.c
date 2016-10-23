777bcdb26f43421be7f8e2a7812c4d2d272ecc03
Jonathan W. Taylor <elderling@gmail.com>
2016-10-23 14:19:21 -0500

    Removed wolves.h since it was only adding complication

--- wolves/wolves.c
+++ wolves/wolves.c
@@ -1,11 +1,31 @@
 
+#include <stdlib.h>
+
 #define DEBUG 1
 #ifdef DEBUG
  #include <stdio.h>
 #endif
 
-#include <stdlib.h>
-#include "wolves.h"
+typedef enum { wait, on, off } animation;
+
+typedef struct {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+    unsigned char brightness;
+    unsigned int frame;
+    unsigned int end_frame;
+    animation anim;
+} dyn_pixl;
+
+void do_wait(dyn_pixl * pixl);
+void do_on(dyn_pixl * pixl);
+void do_off(dyn_pixl * pixl);
+void render_pixl(dyn_pixl * pixl);
+void update_pixls( dyn_pixl pixls[] );
+void render_pixls( dyn_pixl pixls[] );
+void random_anim(dyn_pixl * pixl);
+long random_at_most(long max) ;
 
 
 #define NUMBER_OF_PIXLS 3
