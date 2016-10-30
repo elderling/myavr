fdb041893b04f3cb1951a181e6414d12cca4fcad
Jonathan W. Taylor <elderling@gmail.com>
2016-10-30 13:10:11 -0500

    Updated so that animations don't automatically loop.

--- halloween/halloween.ino
+++ halloween/halloween.ino
@@ -8,29 +8,41 @@ Adafruit_NeoPixel strip =
 Adafruit_NeoPixel (NUMBER_OF_PIXLS, PIN, NEO_GRB + NEO_KHZ800);
 
 RGB red = { 64, 0, 0 };
+RGB green = { 0, 64, 0 };
 
 #define TOTAL_INSTRUCTIONS 4
 #define NUM_PIXLS 8
 dyn_pixl pixls[NUM_PIXLS];
 
+void init_pixls ();
 void
 setup ()
 {
   strip.begin ();
-  int i;
-  for (i = 0; i < NUM_PIXLS/2; i++  )
-    {
-      pixls[i*2].rgb = &red;
-      pixls[i*2].frame = 0;
-      pixls[i*2].brightness = 0;
-      pixls[i*2].inst = get_prg_instruction (i % TOTAL_INSTRUCTIONS);
-      pixls[i*2].prg_counter = (i % TOTAL_INSTRUCTIONS);
+  init_pixls ();
+  return;
+}
 
-      pixls[i*2+1].rgb = &red;
-      pixls[i*2+1].frame = 0;
-      pixls[i*2+1].brightness = 0;
-      pixls[i*2+1].inst = get_prg_instruction (i % TOTAL_INSTRUCTIONS);
-      pixls[i*2+1].prg_counter = (i % TOTAL_INSTRUCTIONS);
+void
+init_pixls ()
+{
+  int i;
+  for (i = 0; i < NUM_PIXLS; i++) {
+      pixls[i].rgb = &green;
+      pixls[i].frame = 0;
+      pixls[i].brightness = 0;
+      pixls[i].inst = get_prg_instruction (0);
+      pixls[i].prg_counter = 0;
+  }
+    
+  //int randm = random_at_most(7);
+  for (i = 0; i <= 4; i++)
+    {
+      pixls[i].rgb = &red;
+      pixls[i].frame = 0;
+      pixls[i].brightness = 0;
+      pixls[i].inst = get_prg_instruction (0);
+      pixls[i].prg_counter = 0;
     }
   return;
 }
@@ -40,19 +52,24 @@ loop ()
 {
   unsigned char red, green, blue;
 
-  int i;
-  for (i = 0; i < NUM_PIXLS; i++)
+  int n, i;
+  for (n = 0; n < 450; n++)
     {
-      update_pixl (&pixls[i]);
+      for (i = 0; i < NUM_PIXLS; i++)
+        {
+          update_pixl (&pixls[i]);
 
-      red = pixls[i].brightness * pixls[i].rgb->red;
-      green = pixls[i].brightness * pixls[i].rgb->green;
-      blue = pixls[i].brightness * pixls[i].rgb->blue;
+          red = pixls[i].brightness * pixls[i].rgb->red;
+          green = pixls[i].brightness * pixls[i].rgb->green;
+          blue = pixls[i].brightness * pixls[i].rgb->blue;
 
-      strip.setPixelColor (i, strip.Color (red, green, blue));
+          strip.setPixelColor (i, strip.Color (red, green, blue));
+        }
+      strip.show ();
+
+      // put your main code here, to run repeatedly:
+      delay (10);
     }
-  strip.show ();
 
-  // put your main code here, to run repeatedly:
-  delay (10);
+  init_pixls ();
 }
